22007ee9749425f6dd6346657ba62db95d6ec9f8
RodrigoSandon <66277833+RodrigoSandon@users.noreply.github.com>
2021-12-05 15:07:51 -0500

    preldsc formatting

--- MB-PD_Association_Study_Pipeline/3_LD_Score_Regression/preLDSC_formatting.py
+++ MB-PD_Association_Study_Pipeline/3_LD_Score_Regression/preLDSC_formatting.py
@@ -8,16 +8,26 @@ Created on Sat Jul 17 19:20:39 2021
 
 import pandas as pd
 
-def formatSumStatsForLDSC_PD (csvPath):
+
+def formatSumStatsForLDSC_PD(csvPath) -> pd.DataFrame:
     df = pd.read_csv(csvPath)
-    newDf = df[['SNP', 'A1', 'A2', 'b','se', 'N_cases', 'p']].copy()
+    newDf = df[['SNP', 'A1', 'A2', 'b', 'se', 'N_cases', 'p']].copy()
     newDf.insert(3, 'Zscore', newDf['b'] / newDf['se'], True)
-    newDf = newDf.drop(['b','se'], axis = 1)
-    newDf = newDf.rename(columns={'N_cases' : 'N', 'SNP' : 'snpid', 'p' : 'P-value'}) # Now have: snpid A1 A2 Zscore N P-value
-    
-def formatSumStatsForLDSC_MB (csvPath):
+    newDf = newDf.drop(['b', 'se'], axis=1)
+    # Now have: snpid A1 A2 Zscore N P-value
+    newDf = newDf.rename(
+        columns={'N_cases': 'N', 'SNP': 'snpid', 'p': 'P-value'})
+
+    return newDf
+
+
+def formatSumStatsForLDSC_MB(csvPath):
     df = pd.read_csv(csvPath)
-    
-    
-chrID_rsIDs = pd.read_csv("/Volumes/T7Touch/NIHSummer2021/Data/EuropeanParkinsons/chrPosRs.tab", sep='\t', lineterminator='\r', header = None)
-chrID_rsIDs.head()
+
+
+"""chrID_rsIDs = pd.read_csv("/Users/rodrigosandon/Documents/GitHub/ParkisonsMicrobiomeStudy/MB-PD_Association_Study_Pipeline/3_LD_Score_Regression/chrPosRs.tab",
+                          sep='\t', lineterminator='\r', header=None)"""
+# chrID_rsIDs.head()
+
+result = formatSumStatsForLDSC_PD(
+    "/Users/rodrigosandon/Documents/GitHub/ParkisonsMicrobiomeStudy/MB-PD_Association_Study_Pipeline/3_LD_Score_Regression/chrPosRs.tab")
